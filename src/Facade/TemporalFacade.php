6a30dce0b805cf1ba1ee3adb895aff6c158de5cd
Stanislau Komar <stanislau_komar@epam.com>
2022-12-25 11:48:17 +0400

    v1.0

--- src/Facade/TemporalFacade.php
+++ src/Facade/TemporalFacade.php
@@ -2,12 +2,14 @@
 
 namespace Micro\Plugin\Temporal\Facade;
 
+use Micro\Plugin\Temporal\Activity\Factory\ActivityStubFactoryInterface;
 use Micro\Plugin\Temporal\Configuration\TemporalPluginConfigurationInterface;
 use Micro\Plugin\Temporal\Worker\Factory\WorkerFactoryInterface;
 use Micro\Plugin\Temporal\Workflow\Client\Repository\ClientRepositoryInterface;
 use Temporal\Client\WorkflowClientInterface;
 use Temporal\Client\WorkflowOptions;
 use Temporal\Internal\Support\Options;
+use Temporal\Internal\Workflow\ActivityProxy;
 use Temporal\Worker\WorkerFactoryInterface as TemporalWorkerFactoryInterface;
 
 class TemporalFacade implements TemporalFacadeInterface
@@ -15,10 +17,12 @@ class TemporalFacade implements TemporalFacadeInterface
     /**
      * @param ClientRepositoryInterface $clientRepository
      * @param WorkerFactoryInterface $workerFactory
+     * @param ActivityStubFactoryInterface $activityStubFactory
      */
     public function __construct(
         private readonly ClientRepositoryInterface $clientRepository,
-        private readonly WorkerFactoryInterface $workerFactory
+        private readonly WorkerFactoryInterface $workerFactory,
+        private readonly ActivityStubFactoryInterface $activityStubFactory
     )
     {
     }
@@ -29,6 +33,14 @@ class TemporalFacade implements TemporalFacadeInterface
     public function workflowClient(string $clientName = TemporalPluginConfigurationInterface::CLIENT_DEFAULT): WorkflowClientInterface
     {
         return $this->clientRepository->client($clientName);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createActivityStub(string $activityInterface): ActivityProxy
+    {
+        return $this->activityStubFactory->create($activityInterface);
     }
 
     /**
--- src/Facade/TemporalFacadeInterface.php
+++ src/Facade/TemporalFacadeInterface.php
@@ -5,6 +5,7 @@ namespace Micro\Plugin\Temporal\Facade;
 use Micro\Plugin\Temporal\Configuration\TemporalPluginConfigurationInterface;
 use Temporal\Client\WorkflowClientInterface;
 use Temporal\Internal\Support\Options;
+use Temporal\Internal\Workflow\ActivityProxy;
 use Temporal\Worker\WorkerFactoryInterface as TemporalWorkerFactoryInterface;
 
 interface TemporalFacadeInterface
@@ -15,6 +16,15 @@ interface TemporalFacadeInterface
      * @return WorkflowClientInterface
      */
     public function workflowClient(string $clientName = TemporalPluginConfigurationInterface::CLIENT_DEFAULT): WorkflowClientInterface;
+
+    /**
+     * @template T
+     *
+     * @param class-string<T> $activityInterface
+     *
+     * @return T
+     */
+    public function createActivityStub(string $activityInterface): ActivityProxy;
 
     /**
      * @return Options
